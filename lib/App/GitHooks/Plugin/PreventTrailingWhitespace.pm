3c03fdffab785fe661e93a1f557fabb7b7880cc8
Ben Arwin <barwin@cpan.org>
2014-06-10 00:19:56 +0000

    Import of BARWIN/App-GitHooks-Plugin-PreventTrailingWhitespace-v1.0.1 from CPAN.
    gitpan-cpan-distribution: App-GitHooks-Plugin-PreventTrailingWhitespace
    gitpan-cpan-version:      v1.0.1
    gitpan-cpan-path:         BARWIN/App-GitHooks-Plugin-PreventTrailingWhitespace-v1.0.1.tar.gz
    gitpan-cpan-author:       BARWIN
    gitpan-cpan-maturity:     released

--- lib/App/GitHooks/Plugin/PreventTrailingWhitespace.pm
+++ lib/App/GitHooks/Plugin/PreventTrailingWhitespace.pm
@@ -23,11 +23,11 @@ Prevent pesky trailing whitespace from being committed.
 
 =head1 VERSION
 
-Version 1.0.0
+Version 1.0.1
 
 =cut
 
-our $VERSION = '1.0.0';
+our $VERSION = '1.0.1';
 
 
 =head1 METHODS
